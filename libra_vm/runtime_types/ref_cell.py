a59204250c815d6f9f7c0f99181720dbe9ca260e
yuan_xy <yuan_xin_yu@hotmail.com>
2020-02-20 14:22:01 +0800

    canoser serialize of RefCell

--- libra_vm/runtime_types/ref_cell.py
+++ libra_vm/runtime_types/ref_cell.py
@@ -8,10 +8,10 @@ class BorrowFlag(IntEnum):
 class Ref:
     def __init__(self, obj, cell):
         self.v0 = obj
-        self.cell = cell
+        self._cell = cell
 
     def __del__(self):
-        self.cell.unborrow()
+        self._cell.unborrow()
 
 class RefMut:
     def __init__(self, obj, cell):
@@ -31,7 +31,7 @@ class RefCell:
         assert self.flag == BorrowFlag.UNUSED
         return self.v0
 
-    def borrow(self):
+    def borrow(self) -> Ref:
         assert self.flag >= BorrowFlag.UNUSED
         self.flag += 1
         return Ref(self.v0, self)
@@ -40,12 +40,48 @@ class RefCell:
         assert self.flag > BorrowFlag.UNUSED
         self.flag -= 1
 
-    def borrow_mut(self):
+    def borrow_mut(self) -> RefMut:
         assert self.flag == BorrowFlag.UNUSED
         self.flag = BorrowFlag.WRITE
         return RefMut(self.v0, self)
+
+    def borrow_mut_set(self, value):
+        refmut = self.borrow_mut()
+        refmut.cell.v0 = value
 
     def unborrow_mut(self):
         assert self.flag == BorrowFlag.WRITE
         self.flag = BorrowFlag.UNUSED
 
+
+from canoser.types import type_mapping
+from canoser.base import Base
+
+
+class RefCellCanoser(RefCell, Base):
+    delegate_type = 'delegate'
+
+    @classmethod
+    def dtype(cls):
+        return type_mapping(cls.delegate_type)
+
+    @classmethod
+    def encode(cls, value):
+        return cls.dtype().encode(value.v0)
+
+    @classmethod
+    def decode(cls, cursor):
+        v0 = cls.dtype().decode(cursor)
+        return cls(v0)
+
+    @classmethod
+    def check_value(cls, value):
+        cls.dtype().check_value(value.v0)
+
+    def to_json_serializable(self):
+        return cls.dtype().to_json_serializable(self.v0)
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        return self.v0 == other.v0 and self.borrow == other.borrow
--- test/vm_runtime_types/test_ref_cell.py
+++ test/vm_runtime_types/test_ref_cell.py
@@ -1,9 +1,18 @@
 from libra_vm.runtime_types.ref_cell import *
+from canoser import DelegateT
 import pytest
 
+class StrRefCell(RefCellCanoser):
+    delegate_type = str
+
+def test_canoser():
+    x = StrRefCell("asdf")
+    ser = x.serialize()
+    x2 = StrRefCell.deserialize(ser)
+    assert x.v0 == x2.v0
 
 def test_refcell():
-    x = "asdf"
+    x = ['a', 'b', 'c']
     refcell = RefCell(x)
     y = refcell.into_inner()
     assert y == x
@@ -11,6 +20,10 @@ def test_refcell():
     b2 = refcell.borrow()
     assert refcell.flag == 2
     assert b1.v0 == b2.v0
+    #WARNING: borrow mutable obj, you can modify the obj, BUT you should avoid do this.
+    b1.v0.append("d")
+    assert b2.v0 == ['a', 'b', 'c', 'd']
+    assert refcell.v0 == ['a', 'b', 'c', 'd']
     with pytest.raises(AssertionError):
         refcell.borrow_mut()
     del b1
@@ -21,11 +34,33 @@ def test_refcell():
         mb1 = refcell.borrow_mut()
         assert refcell.flag == -1
         assert mb1.v0 == x
+        mb1.v0 = "change by value"
+        assert refcell.v0 == ['a', 'b', 'c', 'd']
+        mb1.cell.v0 = "change by ref"
+        assert refcell.v0 == "change by ref"
+        mb1.cell.v0 = x
         with pytest.raises(AssertionError):
             refcell.borrow_mut()
         with pytest.raises(AssertionError):
             refcell.borrow()
+    lambda0()
+    lambda0()
     assert refcell.flag == 0
+    refcell2 = refcell
+    assert refcell2.flag == 0
+    ref2 = refcell2.borrow()
+    assert refcell2.flag == 1
+    assert refcell.flag == 1
+    assert id(refcell) == id(refcell2)
+    assert id(refcell.v0) == id(refcell2.v0)
+    with pytest.raises(AssertionError):
+        refcell.borrow_mut()
+    assert refcell2.flag == 1
+    del ref2
+    assert refcell2.flag == 0
+    refcell2.borrow() #borrow, and then throw the return value, so borrow returned.
+    assert refcell2.flag == 0
+
 
 
 
