2eab28128a3de748ffbefe64fd7a545957e27310
yuan_xy <yuan_xin_yu@hotmail.com>
2020-03-01 22:26:29 +0800

    batch replace rsut file to py

---
+++ batch_rust_to_py.py
@@ -0,0 +1,28 @@
+import sys, os, json
+from os import listdir
+from os.path import isfile, join, abspath, dirname
+import subprocess
+
+
+def batch_of_dir(extern_dir):
+    curdir = dirname(__file__)
+    path = join(curdir, extern_dir)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if(file.endswith(".py")):
+                rs_src = abspath(join(root,file))
+                print(rs_src)
+                cmds = ["/bin/bash", "/usr/bin/rust2py.sh", rs_src]
+                subprocess.run(cmds)
+                #subprocess.run(cmds, cwd=curdir, check=True)
+
+def main():
+    if len(sys.argv) <= 1:
+        print(f"Usage: python3 {sys.argv[0]} dir")
+        return
+    rust_dir = sys.argv[1]
+    batch_of_dir(rust_dir)
+
+
+if __name__ == '__main__':
+    main()
