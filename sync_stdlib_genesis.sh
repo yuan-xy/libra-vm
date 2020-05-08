f2b032af6458f068acde901d582aefd1ab219de2
yuan_xy <yuan_xin_yu@hotmail.com>
2020-05-08 20:09:47 +0800

    add stdlib module soruce

--- sync_stdlib_genesis.sh
+++ sync_stdlib_genesis.sh
@@ -1,4 +1,11 @@
 rm -rf stdlib/staged/*
-cp -rf ../libra/language/stdlib/staged/*  ./stdlib/staged/
+cp -rf ../libra/language/stdlib/staged/*  ./mol/stdlib/staged/
 
-cp ../libra/language/tools/vm-genesis/genesis/genesis.blob vm_genesis/genesis/genesis.blob
+rm -rf ./mol/stdlib/modules/*
+cp -rf ../libra/language/stdlib/modules/*.move  ./mol/stdlib/modules/
+
+# rm -rf ./mol/stdlib/transaction_scripts/*
+# cp -rf ../libra/language/stdlib/transaction_scripts/*.move  ./mol/stdlib/transaction_scripts/
+
+
+cp ../libra/language/tools/vm-genesis/genesis/genesis.blob ./mol/vm_genesis/genesis/genesis.blob
