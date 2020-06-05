61cc0826d3c6fff26bff166b462d9de76d70b01d
yuan_xy <yuan_xin_yu@hotmail.com>
2020-06-05 22:59:13 +0800

    pytest.mark.parametrize for compiler test

--- test/disassembler/test_compile_then_dis.py
+++ test/disassembler/test_compile_then_dis.py
@@ -4,21 +4,39 @@ import os
 from os.path import join, dirname
 import sys
 from pathlib import Path
+import pytest
 
-
-def test_compile_then_disassemble():
+@pytest.mark.parametrize(
+    'compile_args', [
+        ([]),
+        (["--src-map"]),
+    ]
+)
+def test_compile_then_disassemble(capsys, compile_args):
     curdir = dirname(__file__)
     # filename = join(curdir, "../../mol/compiler/ir_stdlib/modules/libra_time.mvir")
     filename = join(curdir, "../../ir-testsuite/option.mvir")
     mvfile = Path(filename).with_suffix(".mv")
+    mvsmfile = Path(filename).with_suffix(".mvsm")
     if mvfile.exists():
         os.remove(mvfile)
+    if mvsmfile.exists():
+        os.remove(mvsmfile)
+
     sys.argv = sys.argv[:1]
     sys.argv.append(filename)
+    sys.argv.extend(compile_args)
     compiler_main()
     assert mvfile.exists()
+    if compile_args == ["--src-map"]:
+        assert mvsmfile.exists()
 
     sys.argv = sys.argv[:1]
     sys.argv.extend(["-b", mvfile.as_posix()])
     dis_main()
+    out = capsys.readouterr().out
+    assert "v: vector" in out
     os.remove(mvfile)
+    if compile_args == ["--src-map"]:
+        assert "public unwrap_or<E: 2>(x: T<E>, e: E): E {" in out
+        os.remove(mvsmfile)
